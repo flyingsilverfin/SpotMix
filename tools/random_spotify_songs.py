6197f6042117160d38b20b0937f1eff853810e7f
Joshua Send <joshuasend1@gmail.com>
2019-02-08 08:54:42 +0000

    tool to partition search space into under-10k chunks

--- tools/random_spotify_songs.py
+++ tools/random_spotify_songs.py
@@ -14,7 +14,7 @@ def search(query, offset=0, limit=10, entity_type="track"):
             "Accept": "application/json",
             "Content-Type": "application/json"
             }
-    response = requests.get("https://api.spotify.com/v1/search?q=\"{0}\"&type={1}&limit={2}&offset={3}".format(query, entity_type, limit, offset), headers=headers)
+    response = requests.get("https://api.spotify.com/v1/search?q={0}&type={1}&limit={2}&offset={3}".format(query, entity_type, limit, offset), headers=headers)
 
     if response.status_code != 200:
         raise Exception(response.json())
@@ -37,48 +37,66 @@ def get_total_tracks_for_search_term(term):
 
 # build up data structure
 
-symbols = "abcdefghijklmnopqrstuvwxyz+"
+alphas = "abcdefghijklmnopqrstuvwxyz"
+nums = "0123456789"
+
+
+country_codes = open("tools/country_codes.txt").readlines()
+registrant_symbols = alphas + nums 
+year_symbols = nums
+code_symbols = nums
+
+prefix_length_next_char_choices = {
+        2: registrant_symbols, 3: registrant_symbols, 4: registrant_symbols,
+        5: year_symbols, 6: year_symbols, 
+        7:code_symbols, 8:code_symbols, 9: code_symbols, 10: code_symbols, 11: code_symbols
+}
+
+def search_term(isrc_prefix):
+    return "isrc:{0}*".format(isrc_prefix)
 
 under_10k_term_counts = {}
 
-counts = {s:None for s in symbols}
+new_prefixes = [c.strip() for c in country_codes]
 
 iteration = 0
-while len(counts) > 0:
-    # expand all the remaining terms by one character
-    new_terms = []
-    for term in list(counts.keys()):
-        new_terms += [term + s for s in symbols]
-
-    # save the expanded terms counts 
+while len(new_prefixes) > 0:
     counts = {}
-    for i, term in enumerate(new_terms):
+    for i, prefix in enumerate(new_prefixes):
+        prefix = prefix.upper()
 
         # in case we hit the request limit, put in a loop
         failed = True
         while failed:
             try:
-                counts[term] = get_total_tracks_for_search_term(term)
-                print("{0}/{1}. {2} - {3}".format(i, len(new_terms), term, counts[term]))
+                counts[prefix] = get_total_tracks_for_search_term(search_term(prefix))
+                print("{0}/{1}. {2} - {3}".format(i, len(new_prefixes), prefix+"*", counts[prefix]))
                 failed = False
             except Exception:
                 time.sleep(1)
                 continue
 
-    for term in list(counts.keys()):
-        if counts[term] < 10000:
-            under_10k_term_counts[term] = counts[term] 
-            del counts[term]
+    for prefix in list(counts.keys()):
+        if counts[prefix] < 10000:
+            under_10k_term_counts[prefix] = counts[prefix] 
+            del counts[prefix]
 
     print("Number of terms under 10k: {0}, sum: {1}".format(len(under_10k_term_counts), sum(under_10k_term_counts.values())))
 
     print("Number of terms over 10k: {0}, sum: {1}".format(len(counts), sum(counts.values())))
 
-
     iteration += 1
 
-    json.dump(under_10k_term_counts, open("tools/data/under_10k_iter_{0}.json".format(iteration), 'w'))
-    json.dump(counts, open("tools/data/over_10k_iter_{0}.json".format(iteration), 'w'))
+    json.dump(under_10k_term_counts, open("tools/data/isrc_prefixed/under_10k_iter_{0}.json".format(iteration), 'w'))
+    json.dump(counts, open("tools/data/isrc_prefixed/over_10k_iter_{0}.json".format(iteration), 'w'))
+
+
+    # expand all the remaining terms by one character
+    new_prefixes= []
+    for term in list(counts.keys()):
+        next_choices = prefix_length_next_char_choices[len(term)]
+        new_prefixes += [term + s for s in next_choices]
+
 
 
 
